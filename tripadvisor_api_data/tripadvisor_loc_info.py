a98104dacb6b128b511b2f7939c861148d8116ef
root <root@DESKTOP-CMJTCKP.localdomain>
2024-06-11 10:23:19 -0400

    Initial commit

--- tripadvisor_api_data/tripadvisor_loc_info.py
+++ tripadvisor_api_data/tripadvisor_loc_info.py
@@ -1,23 +1,28 @@
 import requests
+import json
+import os
 
 key = '3265045EBF3847FCB2DCB6213C1FE9EB'
-
 BASE_URL = 'https://api.content.tripadvisor.com/api/v1/location'
 
-location_id = '258730'
+file_path = os.path.join('tripadvisor_api_data', 'extracted_data.json')
+with open(file_path , 'r') as file:
+    extracted_data = json.load(file)
 
-url = f"{BASE_URL}/{location_id}/details?key={key}"
+for id in extracted_data:
+    location_id = id['location_id']
+    url = f"{BASE_URL}/{location_id}/details?key={key}"
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        id['web_url'] = data.get('web_url', 'N/A')
+        id['category'] = data.get('category', 'N/A')
+    else:
+        print(f"Failed to retrieve data: {response.status_code}")
+        print(response.json())
 
-headers = {
-    'X-TripAdvisor-API-Key': key,
-    'Content-Type': 'application/json',
-}
-
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Failed to retrieve data: {response.status_code}")
-    print(response.json())
+with open(file_path, 'w') as file:
+    json.dump(extracted_data, file, indent=4)
