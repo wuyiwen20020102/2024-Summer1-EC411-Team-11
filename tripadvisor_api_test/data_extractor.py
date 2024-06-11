a30bf3627a1240422435e0383c45c6159f27f8cb
root <root@DESKTOP-CMJTCKP.localdomain>
2024-06-11 08:35:52 -0400

    Initial commit

--- tripadvisor_api_test/data_extractor.py
+++
@@ -1,9 +0,0 @@
-def extract_data(original_data):
-    extracted_data = []
-    for location in original_data['data']:
-        extracted_data.append({
-            'location_id' : location['location_id'],
-            'name' : location['name'],
-            'address_string' : location['address_obj']['address_string']
-        })
-    return extracted_data
--- tripadvisor_api_test/tripadvisor_loc_search.py
+++
@@ -1,8 +0,0 @@
-import json
-from tripadvisorapi.api import TripadvisorApi
-
-key = '3265045EBF3847FCB2DCB6213C1FE9EB'
-api = TripadvisorApi(key)
-res = api.location_search("Los Angeles")
-res_json = json.loads(res.text)
-print(res_json)
