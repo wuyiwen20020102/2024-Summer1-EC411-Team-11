2b0829682f27b6a0d8ef43f9ac926f7756fcea30
root <root@DESKTOP-CMJTCKP.localdomain>
2024-06-16 13:30:44 -0400

    Initial commit

---
+++ login_signup_page/auth.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+
+const GOOGLE_CLIENT_ID = '412967482250-gb8bvpp05qq2d8seuhui8qjcl03ot54u.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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'; //密钥放这里
+
+
+
+passport.use(new GoogleStrategy({
+    clientID:     GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    console.log('AccessToken:', accessToken);
+    console.log('Profile:', profile);
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user,done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user,done) {
+    done(null, user);
+});
