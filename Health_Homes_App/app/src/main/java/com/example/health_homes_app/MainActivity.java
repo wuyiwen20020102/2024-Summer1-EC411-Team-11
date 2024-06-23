87c5a8a319ee5e6bfd072c25e414b936da304b60
root <root@DESKTOP-CMJTCKP.localdomain>
2024-06-23 15:49:11 -0400

    Initial commit

---
+++ Health_Homes_App/app/src/main/java/com/example/health_homes_app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.health_homes_app;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Enable JS, HTML script
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.addJavascriptInterface(new WebAppInterface(), "AndroidInterface");
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    private class WebAppInterface{
+        @JavascriptInterface
+        public void receiveData(String user, String password){
+            Toast.makeText(MainActivity.this, "User: " + user + "\nPassword: " + password, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+}
